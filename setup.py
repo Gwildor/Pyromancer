167bf95860f5f7cb6939860e1701475ae1c233ac
Gwildor Sok <gwildor@wend.nl>
2014-03-14 17:41:17 +0100

    Release as v0.2

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import pyromancer
 setup(
     name='Pyromancer',
     version=pyromancer.__version__,
-    packages=['pyromancer', 'pyromancer.commands'],
-    description='Simple IRC bot implementation / framework',
-    long_description=open('README.md').read(),
+    packages=['pyromancer', 'pyromancer.commands', 'pyromancer.test'],
+    description='Simple framework for creating IRC bots',
+    long_description=open('README.rst').read(),
     author='Gwildor Sok',
     author_email='gwildorsok@gmail.com',
     url='https://github.com/Gwildor/Pyromancer',
