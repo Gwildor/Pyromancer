0d14c49e351a72b90990086d980ee32d0b0c737c
Gwildor Sok <gwildorsok@gmail.com>
2014-03-22 21:52:58 +0100

    Fix problem with setup preventing installation from Github

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 
 import pyromancer
 
+try:
+    long_description = open('README.rst').read()
+except FileNotFoundError:
+    long_description = open('README.md').read()
+
 setup(
     name='Pyromancer',
     version=pyromancer.__version__,
     packages=['pyromancer', 'pyromancer.commands', 'pyromancer.test'],
     description='Simple framework for creating IRC bots',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     author='Gwildor Sok',
     author_email='gwildorsok@gmail.com',
     url='https://github.com/Gwildor/Pyromancer',
